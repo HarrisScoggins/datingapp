1e4e6f07114f8083bb75578c683bcdf7f27623b9
Harris Scoggins <harrisscoggins@gmail.com>
2020-04-15 15:19:27 -0400

    Enter

--- datingapp.api/Startup.cs
+++ datingapp.api/Startup.cs
@@ -38,7 +38,7 @@ namespace datingapp.api
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
+            { 
                 app.UseDeveloperExceptionPage();
             }
             else
