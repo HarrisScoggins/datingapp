0de7ecfe4bc7eaf1c3b945991ec7892835bb93a5
Harris Scoggins <harrisscoggins@gmail.com>
2020-05-09 14:47:43 -0400

    5/9

--- datingapp.api/Dtos/UserForDetailedDto.cs
+++ datingapp.api/Dtos/UserForDetailedDto.cs
@@ -6,12 +6,12 @@ namespace datingapp.api.Dtos
 {
     public class UserForDetailedDto
     {
-        
+
         public int Id { get; set; }
         public string Username { get; set; }
 
         public string Gender { get; set; }
-        public int  Age { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
@@ -20,6 +20,7 @@ namespace datingapp.api.Dtos
         public string Intrests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-         public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
     }
 }
